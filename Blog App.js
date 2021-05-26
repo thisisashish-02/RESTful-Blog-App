58ae74b8dccb506b0f9d110b945d91cb83fa1e6b
ASHISH <65739459+thisisashish-02@users.noreply.github.com>
2021-05-26 11:29:39 +0530

    Edit blogs

--- Blog App.js
+++ Blog App.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const path = require('path');
+const methodOverride = require('method-override');
 const app = express();
 const { v4: uuid } = require('uuid');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 let comments = [
     {
@@ -44,6 +52,20 @@ app.get('/comments/:id', (req, res) => {
     const { id } = req.params;
     const comment = comments.find(c => c.id === id);
     res.render('comments/show', { comment });
+})
+
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', { comment });
+})
+
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const newCommentText = req.body.comment;
+    const comment = comments.find(c => c.id === id);
+    comment.comment = newCommentText;
+    res.redirect('/comments');
 })
 
 app.listen(3000, () => {
